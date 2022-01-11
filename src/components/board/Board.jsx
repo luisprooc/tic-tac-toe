b2b1654df4264f7cf15405ba375b85c327e98c48
Luis Rosario <luisprooc@gmail.com>
2022-01-11 18:15:23 -0400

    feat: add dinamic tabl

--- src/components/board/Board.jsx
+++ src/components/board/Board.jsx
@@ -1,38 +1,124 @@
 import { Table } from "reactstrap";
-import "./board.css"
+import "./board.css";
+import React, { useState } from "react";
 
 const Board = () => {
+  const [table] = useState([
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ]);
+
+  const selectBox = (e) => {
+    console.log(e.target.id);
+  };
+
   return (
     <Table borderless className="board-table">
       <thead>
         <tr>
-          <th
-            className="board-border-right board-border-bottom"
-          >
-           
-          </th>
-          <th
-            className="board-border-bottom board-border-right"
-          ></th>
-          <th className="board-border-bottom"></th>
+          {table[0].map((value, index) => {
+            return (
+              <>
+                {index === 0 && (
+                  <th
+                    className="board-border-right board-border-bottom"
+                    id="0,0"
+                    onClick={selectBox}
+                  >
+                    {value}
+                  </th>
+                )}
+
+                {index === 1 && (
+                  <th
+                    className="board-border-bottom board-border-right"
+                    id="0,1"
+                    onClick={selectBox}
+                  >
+                    {value}
+                  </th>
+                )}
+                {index === 2 && (
+                  <th
+                    className="board-border-bottom"
+                    id="0,2"
+                    onClick={selectBox}
+                  >
+                    {value}
+                  </th>
+                )}
+              </>
+            );
+          })}
         </tr>
         <tr>
-          <th
-            className="board-border-bottom board-border-right"
-          >
-            
-          </th>
-          <th
-            className="board-border-bottom board-border-right"
-          ></th>
-          <th className="board-border-bottom"></th>
+          {table[1].map((value, index) => {
+            return (
+              <>
+                {index === 0 && (
+                  <th
+                    className="board-border-bottom board-border-right"
+                    id="1,0"
+                    onClick={selectBox}
+                  >
+                    {value}
+                  </th>
+                )}
+                {index === 1 && (
+                  <th
+                    className="board-border-bottom board-border-right"
+                    id="1,1"
+                    onClick={selectBox}
+                  >
+                    {value}
+                  </th>
+                )}
+                {index === 2 && (
+                  <th
+                    className="board-border-bottom"
+                    id="1,2"
+                    onClick={selectBox}
+                  >
+                    {value}
+                  </th>
+                )}
+              </>
+            );
+          })}
         </tr>
         <tr>
-          <th className="board-border-right">
-            
-          </th>
-          <th className="board-border-right"></th>
-          <th></th>
+          {table[2].map((value, index) => {
+            return (
+              <>
+                {index === 0 && (
+                  <th
+                    className="board-border-right"
+                    id="2,0"
+                    onClick={selectBox}
+                  >
+                    {value}
+                  </th>
+                )}
+
+                {index === 1 && (
+                  <th
+                    className="board-border-right"
+                    id="2,1"
+                    onClick={selectBox}
+                  >
+                    {value}
+                  </th>
+                )}
+
+                {index === 2 && (
+                  <th id="2,0" onClick={selectBox}>
+                    {value}
+                  </th>
+                )}
+              </>
+            );
+          })}
         </tr>
       </thead>
     </Table>
